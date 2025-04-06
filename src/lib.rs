027150de3314b330160fa2a69f8da36855c5379c
gwenn <gtreguier@gmail.com>
2025-04-06 14:46:21 +0200

    Use `doc_auto_cfg` instead of `doc_cfg`

--- src/lib.rs
+++ src/lib.rs
@@ -28,7 +28,7 @@
 //! [Paul Williams' ANSI parser state machine]: https://vt100.net/emu/dec_ansi_parser
 #![deny(clippy::all, clippy::if_not_else, clippy::enum_glob_use)]
 #![cfg_attr(not(feature = "std"), no_std)]
-#![cfg_attr(docsrs, feature(doc_cfg))]
+#![cfg_attr(docsrs, feature(doc_auto_cfg))]
 
 use core::mem::MaybeUninit;
 use core::str;
@@ -39,7 +39,6 @@ use arrayvec::ArrayVec;
 mod params;
 
 #[cfg(feature = "ansi")]
-#[cfg_attr(docsrs, doc(cfg(feature = "ansi")))]
 pub mod ansi;
 pub use params::{Params, ParamsIter};
 
